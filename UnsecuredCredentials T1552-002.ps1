15a7fecfaa392c32d672a70ebb10a00c842b7bff
saitejach96 <saiteja.ch96@gmail.com>
2021-02-08 13:45:41 +0530

    Add files via upload

---
+++ UnsecuredCredentials T1552-002.ps1
@@ -0,0 +1,5 @@
+﻿
+Get-ChildItem -Path HKCU:\ | Select-Object Name >> C:\Users\SAITEJA\Desktop\Output.csv
+Write-Output $a.PSChildName
+    $subkeys = (Get-ItemProperty  $a.pspath)
+    Write-Output $subkeys.LastDateTime_EndScript
