5c04be6ae0bd4f31736785bec673b56eac30b2df
saitejach96 <saiteja.ch96@gmail.com>
2021-02-08 14:02:06 +0530

    Delete UnsecuredCredentials T1552-002.ps1

--- UnsecuredCredentials T1552-002.ps1
+++
@@ -1,5 +0,0 @@
-﻿
-Get-ChildItem -Path HKCU:\ | Select-Object Name >> C:\Users\SAITEJA\Desktop\Output.csv
-Write-Output $a.PSChildName
-    $subkeys = (Get-ItemProperty  $a.pspath)
-    Write-Output $subkeys.LastDateTime_EndScript
