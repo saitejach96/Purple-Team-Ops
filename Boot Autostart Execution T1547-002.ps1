246dbbb2e7388d9889ce71d7b57eba2d8ecf83f4
saitejach96 <saiteja.ch96@gmail.com>
2021-02-08 08:29:24 +0530

    Add files via upload

---
+++ Boot Autostart Execution T1547-002.ps1
@@ -0,0 +1,5 @@
+﻿$trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
+$trigger = New-JobTrigger -AtLogOn -RandomDelay 00:00:30
+
+Register-ScheduledJob -Trigger $trigger -FilePath C:\WINDOWS\system32\notepad.ps1 -Name T1547-002-1
+Register-ScheduledJob -Trigger $trigger -FilePath C:\WINDOWS\system32\notepad.ps1 -Name T1547-002-2
