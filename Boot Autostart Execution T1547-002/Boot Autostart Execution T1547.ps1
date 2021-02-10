516435924fb1b986b29c9c2248461c1a3d7fc4d1
saitejach96 <saiteja.ch96@gmail.com>
2021-02-10 16:16:56 +0530

    Add files via upload

---
+++ Boot Autostart Execution T1547-002/Boot Autostart Execution T1547.ps1
@@ -0,0 +1,13 @@
+﻿#$trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
+#$trigger = New-JobTrigger -AtLogOn -RandomDelay 00:00:30
+#Register-ScheduledJob -Trigger $trigger -FilePath C:\WINDOWS\system32\notepad.ps1 -Name T1547-002-1
+#Register-ScheduledJob -Trigger $trigger -FilePath C:\WINDOWS\system32\notepad.ps1 -Name T1547-002-2
+#$val = Get-Item -path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run-Name
+#Set-Itemproperty -path 'HKLM\SYSTEM\CurrentControlSet\Control\Lsa\' -Name 'Authentication Packages' -value $val+'C:\Program Files\VMware\VMware Tools\vmtoolsd.exe'
+
+
+$Val=Get-ItemPropertyValue -path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\Audit\AuditPolicy\ -Name AuditPolicySD
+
+Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\Audit\AuditPolicy\ -Name AuditPolicySD -Value $Val+"Hi, My DLL Path"
+
+
